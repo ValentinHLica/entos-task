8cc1f927f8a07e7c6b6a856c7e0c26e42cfe9782
Valentin Lica <licavalentinh@gmail.com>
2023-03-07 12:43:52 +0100

    feature: commented filter helper function

--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -11,47 +11,59 @@ export interface CalculatedShipment {
 export const calculateShipment = ({
   selectedCompanies,
   selectedAreas,
-
+  companies,
   shipment,
   areas,
-  companies,
 }: {
   selectedCompanies: number[];
   selectedAreas: number[];
-
   shipment: Shipment[];
   companies: Company[];
   areas: Area[];
 }): CalculatedShipment[] => {
+  // Store Company IDs and Area IDs into Set's for fast check time
   const companiesIds = new Set(selectedCompanies);
   const areasIds = new Set(selectedAreas);
 
+  // Filter Shipment that have the Selected Company ID and Selected Area ID
   const filteredShipment = shipment.filter(
     (item) =>
+      // If Selected Area IDs is empty do not take this filter into account
       (selectedAreas.length > 0 ? areasIds.has(item.areaId) : true) &&
+      // If Selected Companies IDs is empty do not take this filter into account
       (selectedCompanies.length > 0 ? companiesIds.has(item.companyId) : true)
   );
 
+  // Store an object with keys of Company IDs and value of object with keys for Area IDs and value of the shipment count
   const data: {
     [companyId: number | string]: {
       [areaId: number | string]: number;
     };
   } = {};
 
-  const groupedCompanies = companies.reduce((result, current) => {
-    result[current.companyId] = current.name;
-    return result;
-  }, {} as { [key: number | string]: string });
+  // Group Companies with their respective name
+  const groupedCompanies: { [key: number | string]: string } = {};
 
-  const groupedAreas = areas.reduce((result, current) => {
-    result[current.areaId] = current.state;
-    return result;
-  }, {} as { [key: number | string]: string });
+  companies.forEach((company) => {
+    if (!groupedCompanies[company.companyId])
+      groupedCompanies[company.companyId] = company.name;
+  });
 
+  // Group Areas with their respective name
+  const groupedAreas: { [key: number | string]: string } = {};
+
+  areas.forEach((area) => {
+    if (!groupedAreas[area.areaId]) groupedAreas[area.areaId] = area.state;
+  });
+
+  // Loop through filtered Shipment
   for (const item of filteredShipment) {
+    // Check if Company with Area is in Stored Data Object
     if (data[item.companyId] && data[item.companyId][item.areaId]) {
+      // Add to the shipment counter with +1
       data[item.companyId][item.areaId] = data[item.companyId][item.areaId] + 1;
     } else {
+      // Add value to Stored Data variable
       data[item.companyId] = {
         ...data[item.companyId],
         [item.areaId]: 1,
@@ -61,8 +73,11 @@ export const calculateShipment = ({
 
   const list: CalculatedShipment[] = [];
 
+  // Loop through each Company ID key
   Object.keys(data).forEach((companyKey) => {
+    // Loop through each Area ID key
     Object.keys(data[companyKey]).forEach((areaKey) => {
+      // Add to the list formatted shipment data
       list.push({
         area: groupedAreas[areaKey],
         companyName: groupedCompanies[companyKey],
@@ -71,6 +86,7 @@ export const calculateShipment = ({
     });
   });
 
+  // Sort Data By Shipment Count
   return list.sort(
     (firstItem, secondItem) =>
       secondItem.shipmentCount - firstItem.shipmentCount
